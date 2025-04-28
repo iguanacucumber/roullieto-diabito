7bba34122b17fcabf949343fce04ff229049f034
clauteur <110111288+Clauteur@users.noreply.github.com>
2025-04-28 09:16:47 +0400

    Update utils.py
    ajout de listtostr qui est un convertisseur de liste ou liste de liste en str
    ajout de strtolistoflist qui est un convertisseur de str en liste de liste ou en liste

--- src/utils.py
+++ src/utils.py
@@ -45,3 +45,30 @@ def clear():
         system("cls")
     else:
         system("clear")
+
+def listtostr(liste):
+    lestr = ""
+    for i in range (len(liste)):
+        lestr += "["
+        for j in range (len(liste[i])):
+            lestr += str(liste[i][j])
+            lestr += ","
+        lestr += "]"
+    return lestr
+    
+
+
+def strtolistoflists(s):
+    try:
+        # ast.literal_eval évalue une chaîne de manière sécurisée
+        result = ast.literal_eval(s)
+        if isinstance(result, list) and all(isinstance(sublist, list) for sublist in result):
+            return result
+        else:
+            raise ValueError("La chaîne ne représente pas une liste de listes.")
+    except Exception as e:
+        print(f"Erreur lors de la conversion : {e}")
+        return None
+
+laliste = [[1,2],[1,5]] 
+print (listtostr(laliste))
